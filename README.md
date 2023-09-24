cea3e92a0932b13ed5db5b5ee5330b0f6811bc75
fhasancelik <hasancelikjob@gmail.com>
2023-09-24 08:18:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 8-hs-javascript-giris
---
+++ main.js
@@ -0,0 +1,27 @@
+//Hata ayıklama,JavaScript dosyasının bağlanıp bağlanmadığını kontrolu
+// ve veri Kontrolü İçin Kullanılır
+//console.log('Merhaba Dünya')
+
+/*
+
+// işaret tek bir kod satırını yorum yapar
+
+
+
+*/
+
+// /*  */ bu yazım metodu birden fazla satırı yorum satırı yapar
+
+//===JavaScript Çıktı Operatörleri===//
+
+//Yorum satırına brişey yazdırma için kullnaılır
+//console.log("Merhaba JavaScript");
+
+//Ekrana Yazdırmak İçin
+//document.write("Merhaba JavaScript");
+
+
+//Ekranda açılır bir pencere oluşturmak için
+//alert('Bildirimleri Onaylıyor musunuz ?')
+
+
